15fa8b2293496dfa86fb16370879d90adbcd7356
cupofjoy <eelantung@gmail.com>
2019-10-01 14:26:15 -0500

    fix typo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const compression = require('compression';)
+const compression = require('compression');
 
 if(process.env.NODE_ENV !== 'production') require('dotenv').config();
 
