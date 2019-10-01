4ac4d1bd7f327fb1030c951e972e675f8cf4a537
cupofjoy <eelantung@gmail.com>
2019-10-01 18:19:49 -0500

    edit server.js

--- server.js
+++ server.js
@@ -15,22 +15,21 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true }))
-
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(compression);
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-  })
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 
 app.listen(port, error => {
-  if(error) throw error;
-  console.log('Server running on port ' + port);
+  if (error) throw error;
+  console.log('Server is running on port ' + port);
 });
 
 app.get('/serviceWorker.js', (req, res) => {
