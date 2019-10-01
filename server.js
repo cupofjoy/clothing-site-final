eaa5de183f8b37fc3927d2cd4992cfc10433e467
cupofjoy <eelantung@gmail.com>
2019-10-01 18:11:53 -0500

    updating code to be pwa compliant, added serviceworker, https & updated manifest

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 if(process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -14,10 +15,12 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function(req, res) {
@@ -30,12 +33,16 @@ app.listen(port, error => {
   console.log('Server running on port ' + port);
 });
 
+app.get('/serviceWorker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
+
 app.post('/payment', (req, res) => {
   const body = {
     source: req.body.token.id,
     amount: req.body.amount,
     currency: 'usd'
-  };
+  });
 
   stripe.charges.create(body, (stripeErr, stripeRes) => {
     if(stripeErr) {
