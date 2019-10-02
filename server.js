bd83ffd4524b582f7a2202dc9bc8206f78923ddb
cupofjoy <eelantung@gmail.com>
2019-10-01 19:07:29 -0500

    fix heroku error

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
